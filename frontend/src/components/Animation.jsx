8aef7fdc7e957ac1912f27e1914124d7a9fccc37
harshal mali <20harshalmali@gmail.com>
2024-12-31 13:38:18 +0530

    second commit

--- frontend/src/components/Animation.jsx
+++ frontend/src/components/Animation.jsx
@@ -6,7 +6,7 @@ const Animation = ({ action }) => {
 	useEffect(() => {
 		if (action) {
 			setVisible(true);
-			const timer = setTimeout(() => setVisible(false), 2000);
+			const timer = setTimeout(() => setVisible(false), 4000);
 			return () => clearTimeout(timer);
 		}
 	}, [action]);
@@ -14,7 +14,7 @@ const Animation = ({ action }) => {
 	return (
 		visible && (
 			<div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black">
-				<div className="text-white text-2xl font-bold">
+				<div className="text-white text-center text-2xl font-bold">
 					{action.type === "wicket" ? "Wicket!" : `${action.value} Runs!`}
 					{action.value === 1 && <img src="/number1.gif" alt="1 Run" />}
 					{action.value === 2 && <img src="/number2.gif" alt="2 Runs" />}
